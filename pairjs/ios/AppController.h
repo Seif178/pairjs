8cbd271eb127718bf9bb70b02893cec207052abc
Alex <seif178@gmail.com>
2012-12-13 22:46:37 -0500

    Initial Commit

---
+++ pairjs/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  pairjsAppController.h
+//  pairjs
+//
+//  Created by Alex Lau on 12/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ pairjs/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  pairjsAppController.h
+//  pairjs
+//
+//  Created by Alex Lau on 12/13/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
